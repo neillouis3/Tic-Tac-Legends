f2b728f1a67b430e0b734b1b4dc09f57dff39904
neillouis3 <neil03.castillon@gmail.com>
2024-05-09 08:18:07 -0230

    Initialization

---
+++ main/components/backend/normalMode.py
@@ -0,0 +1,8 @@
+def board():
+    print(f'''
+    {board[0]} | {board[1]} | {board[2]}
+    ---------
+    {board[3]} | {board[4]} | {board[5]}
+    ---------
+    {board[6]} | {board[7]} | {board[8]}
+    ''')
---
+++ main/components/frontend/Button.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet } from 'react-native';
+
+type Props = {
+    children: React.ReactNode;
+    onPress: () => void;
+    style?: any;
+};
+
+export default function Button({
+    children,
+    onPress,
+    style,
+    ...rest
+    }: Props) {
+    return (
+        <TouchableOpacity
+        onPress={onPress}
+        style={[styles.button, style]}
+        {...rest}>
+
+        </TouchableOpacity>
+    );
+}
+
+const styles = StyleSheet.create({
+    button: {
+        padding: 12,
+        borderRadius: 6,
+        backgroundColor: '#0a7ea4',
+        alignItems: 'center',
+    },
+});
