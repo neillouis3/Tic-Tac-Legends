71569f16bf135d365bed858e59473206cc1862b1
neillouis3 <neil03.castillon@gmail.com>
2024-05-11 22:55:07 -0230

    Algo

--- main/components/backend/normalMode.py
+++ main/components/backend/normalMode.py
@@ -5,3 +5,16 @@ def create_board():
 
 
 # Path: Projects/Tic-Tac-Toe/main/components/backend/normalMode.py
+
+def check_winner(board):
+    for i in range(3):
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != ' ':
+            return board[i][0]
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != ' ':
+            return board[0][i]
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != ' ':
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] and board[0][2] != ' ':
+        return board[0][2]
+    return None
+
