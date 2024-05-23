303557352550817e5a407581ebefdb4a2ae82099
neillouis3 <neil03.castillon@gmail.com>
2024-05-22 21:44:18 -0230

    Logo

--- main/app/(entry)/index.tsx
+++ main/app/(entry)/index.tsx
@@ -5,17 +5,17 @@ import {View, Text} from 'react-native';
 export default function HomeScreen() {
   return (
     <ImageBackground source={require('../../assets/images/bg.png')} style={styles.container}>
-    
+      <Image source={require('../../assets/images/logo.png')} 
+        style={styles.logo}
+      />
+
+
+
+
       <Button 
         onPress={() => {}}
-        style={{
-          backgroundColor: 'red',
-          padding: 12,
-          width: 100,
-          height: 50,
-      }}
-      >
-        <Text>Hello</Text>
+        style={styles.button}>
+        <Text>Play</Text>
       </Button>
     </ImageBackground>
   );
@@ -27,5 +27,19 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
     padding: 20,
+  },
+
+  logo: {
+    width: 200,
+    height: 200,
+    marginBottom: 40,
+  },
+
+  button: {
+    padding: 12,
+    borderRadius: 6,
+    backgroundColor: '#0a7ea4',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
