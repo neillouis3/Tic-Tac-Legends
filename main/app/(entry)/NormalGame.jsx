d8037892e8f9dd20fc10f8315b6cc577dda2c4bb
neillouis3 <neil03.castillon@gmail.com>
2024-05-31 16:44:40 +0300

    New Screen

---
+++ main/app/(entry)/NormalGame.jsx
@@ -0,0 +1,10 @@
+
+import { View, Text } from 'react-native';
+
+export default function NormalGame() {
+    return (
+        <View>
+            <Text>Normal Game</Text>
+        </View>
+    );
+}
