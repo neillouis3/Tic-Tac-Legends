b0eeb6a842aa6d1b7d0b338c7156d65f20c56cdb
neillouis3 <neil03.castillon@gmail.com>
2024-05-31 16:38:50 +0300

    Routing

--- main/app/(entry)/selectionPage.tsx
+++ main/app/(entry)/selectionPage.tsx
@@ -1,36 +1,33 @@
 import { ImageBackground, Image, StyleSheet, Platform } from 'react-native';
 import Button from '@/components/frontend/Button';
 import {View, Text} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 export default function SelectionPage() {
+  const navigation = useNavigation();
+
   return (
     <ImageBackground source={require('../../assets/images/bg.png')} style={styles.container}>
 
       <Button 
-        onPress={() => {}}
+        onPress={() => navigation.navigate('NormalGameScreen')}
         style={styles.button}>
-        <Text>Normal Mode</Text>
+        <Text>Normal</Text>
       </Button>
 
       <Button 
-        onPress={() => {}}
+        onPress={() => navigation.navigate('PuzzleGameScreen')}
         style={styles.button}>
-        <Text>5x5</Text>
+        <Text>Puzzles</Text>
       </Button>
 
 
       <Button 
-        onPress={() => {}}
+        onPress={() => navigation.navigate('GameModesScreen')}
         style={styles.button}>
-        <Text>Harder</Text>
+        <Text>Gamemodes</Text>
       </Button>
-
-      <Button 
-        onPress={() => {}}
-        style={styles.button}>
-        <Text>Normal Mode</Text>
-      </Button>
-      
+            
     </ImageBackground>
   );
 }
