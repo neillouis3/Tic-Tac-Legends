4ffda9482c7b4ed58c516718e69af8deb39ddae9
neillouis3 <neil03.castillon@gmail.com>
2024-07-09 23:57:38 +0300

    SVG Transformer for Icons

--- app/(entry)/_layout.tsx
+++ app/(entry)/_layout.tsx
@@ -2,6 +2,7 @@
 import React from 'react';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+
 import {Tabs} from 'expo-router';
 
 
@@ -9,10 +10,38 @@ export default function EntryLayout() {
   const colorScheme = useColorScheme();
 
   return (
-    <Tabs>
-      <Tabs.Screen name="(setting)"  options={{ headerShown: false }} />
-      <Tabs.Screen name="(game)"  options={{ headerShown: false }} />
-      <Tabs.Screen name="(profile)"  options={{ headerShown: false }} />
+    <Tabs
+      initialRouteName="(game)"
+      screenOptions={({route}) => ({
+        tabBarIcon: ({focused, size}) => {
+          let iconName;
+          if (route.name === 'Foo') {
+            iconName = focused ? 'home' : 'home-outline';
+          } else if (route.name === 'Bar') {
+            iconName = focused ? 'basketball' : 'basketball-outline';
+          }
+          return <Icon name={iconName} size={size} color={'pink'} />;
+        },
+        tabBarLabelStyle: {
+          color: 'pink',
+        },
+        tabBarStyle: {
+          backgroundColor: 'black',
+        },
+      })}
+    >
+      <Tabs.Screen 
+        name="(setting)"  
+        options={{title:"Settings"}} 
+      />
+      <Tabs.Screen 
+        name="(game)"   
+        options={{title:"Game"}}
+      />
+      <Tabs.Screen 
+        name="(profile)"   
+        options={{title:"Profile"}}
+      />
 
     </Tabs>
   );
