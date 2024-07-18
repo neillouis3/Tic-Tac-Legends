8d3f8dfd459a79543a2ebdcdd45bae4fa937ce19
neillouis3 <neil03.castillon@gmail.com>
2024-07-18 12:20:47 +0300

    Normal game AI

--- components/3x3/computer.jsx
+++ components/3x3/computer.jsx
@@ -1,1 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+
+const Computer = ({ board, onComputerMove }) => {
+  useEffect(() => {
+    const makeComputerMove = () => {
+      const availableMoves = board
+        .map((cell, index) => (cell === null ? index : null))
+        .filter(index => index !== null);
+
+      if (availableMoves.length > 0) {
+        const move = availableMoves[Math.floor(Math.random() * availableMoves.length)];
+        const newBoard = board.slice();
+        newBoard[move] = 'O';
+        onComputerMove(newBoard);
+      }
+    };
+
+    if (board.includes(null)) {
+      const timer = setTimeout(makeComputerMove, 1000);
+      return () => clearTimeout(timer);
+    }
+  }, [board, onComputerMove]);
+
+  return null;
+};
+
+Computer.propTypes = {
+  board: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onComputerMove: PropTypes.func.isRequired,
+};
+
+export default Computer;
