4d5cbcf489a1099c437d0bcb7bde03598b828c85
neillouis3 <neil03.castillon@gmail.com>
2024-07-22 13:30:52 +0300

    Styling for settingScreen

--- app/(entry)/(tabs)/settingScreen.tsx
+++ app/(entry)/(tabs)/settingScreen.tsx
@@ -1,7 +1,6 @@
 import { StyleSheet, Platform, Pressable, Linking } from 'react-native';
 import { View, Text } from 'react-native';
 
-
 export default function SettingScreen() {
   const openPrivatePolicy = () => {
     Linking.openURL('https://neillouis3.vercel.app/privatePolicy');
@@ -11,58 +10,56 @@ export default function SettingScreen() {
       <Text style={styles.nameTitle}>Settings</Text>
       <View style={styles.minorContainer}>
         <Text style={styles.subTitle}>Account</Text>
-        <Text>
-          To be added
-        </Text>
+        <Text>To be added</Text>
       </View>
       <View style={styles.minorContainer}>
         <Text style={styles.subTitle}>Game</Text>
+        <View style={styles.minorContent}>
+          <Text>Sound</Text>
+          <Pressable style={styles.minorContentBtn}>
+            <Text>Contact Us</Text>
+          </Pressable>
+        </View>
+        <View style={styles.minorContent}>
+          <Text>Music</Text>
+          <Pressable style={styles.minorContentBtn}>
+            <Text>Contact Us</Text>
+          </Pressable>
+        </View>
+        <View style={styles.minorContent}>
+          <Text>Vibration</Text>
+          <Pressable style={styles.minorContentBtn}>
+            <Text>Contact Us</Text>
+          </Pressable>
+        </View>
       </View>
       <View style={styles.minorContainer}>
         <Text style={styles.subTitle}>Information</Text>
         <View style={styles.minorContent}>
-          <Text>
-            Help & Support
-          </Text>
+          <Text>Help & Support</Text>
           <Pressable style={styles.minorContentBtn}>
-            <Text>
-              Contact Us
-            </Text>
+            <Text>Contact Us</Text>
           </Pressable>
         </View>
 
         <View style={styles.minorContent}>
-          <Text>
-            Private Policy
-          </Text>
+          <Text>Private Policy</Text>
           <Pressable style={styles.minorContentBtn} onPress={openPrivatePolicy}>
-            <Text>
-              View
-            </Text>
+            <Text>View</Text>
           </Pressable>
         </View>
-        
+
         <View style={styles.minorContent}>
-          <Text>
-            Credits
-          </Text>
+          <Text>Credits</Text>
           <Pressable style={styles.minorContentBtn}>
-            <Text>
-              View
-            </Text>
+            <Text>View</Text>
           </Pressable>
-        </View>        
-        
-        <View style={styles.minorContent}>
-          <Text>
-            Version
-          </Text>
-          <Text>
-              1.0
-          </Text>
-
         </View>
 
+        <View style={styles.minorContent}>
+          <Text>Version</Text>
+          <Text>1.0</Text>
+        </View>
       </View>
 
       <Text style={styles.bottomText}>neillouis3</Text>
@@ -88,7 +85,6 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
-    
   },
 
   minorContentBtn: {
@@ -99,7 +95,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-
 
   bottomText: {
     position: 'absolute',
@@ -120,6 +115,5 @@ const styles = StyleSheet.create({
   subTitle: {
     fontSize: 16,
     fontWeight: 'bold',
-  }
+  },
 });
-
