6119ee253e996df3b122a5c8a74acb1142551196
neillouis3 <neil03.castillon@gmail.com>
2024-08-04 22:50:55 +0300

    Clean up

--- declarations.d.ts
+++ declarations.d.ts
@@ -1,10 +1,4 @@
-declare module '*.svg' {
-    import React from 'react';
-    import { SvgProps } from 'react-native-svg';
-    const content: React.FC<SvgProps>;
-    export default content;
-  }
-  
+
   declare module '*.mp3' {
     const src: string;
     export default src;
